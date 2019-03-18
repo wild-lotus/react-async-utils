466b75974cdf65b78b842168c2f08f052f85d7db
Carlos Ginés <efrel.v2@gmail.com>
2019-03-18 19:11:11 +0100

    Improved README. Added render function tests.

--- src/render.test.js
+++ src/render.test.js
@@ -11,11 +11,21 @@ it('returns `init` render result that provides `aborted` substate given an `Init
   expect(abortedResult).toBe(ABORTED_TEXT);
 });
 
+it('returns `null` if no `init` render provided given an `InitAsync`', () => {
+  const result = render(newInit(), {});
+  expect(result).toBeNull();
+})
+
 it('returns `inProgress` render result given an `InProgressAsync`', () => {
   const TEXT = 'tanfozem';
   const result = render(newInProgress(), { inProgress: () => TEXT });
   expect(result).toBe(TEXT);
 });
+
+it('returns `null` if no `inProgress` render provided given an `InProgressAsync`', () => {
+  const result = render(newInProgress(), {});
+  expect(result).toBeNull();
+})
 
 it('returns `success` render result that provides `payload` and `invalidated` substate given a `SuccessAsync`', () => {
   const SUCCESS_TEXT = 'SUCCESS_jacofduc';
@@ -31,8 +41,18 @@ it('returns `success` render result that provides `payload` and `invalidated` su
   expect(invalidatedResult).toBe(INVALIDATED_TEXT + PAYLOAD_TEXT);
 });
 
+it('returns `null` if no `success` render provided given a `SuccessAsync`', () => {
+  const result = render(newSuccess(), {});
+  expect(result).toBeNull();
+})
+
 it('returns `error` render result given an `ErrorAsync`', () => {
   const TEXT = 'tanfozem';
   const result = render(newError(TEXT), { error: error => error });
   expect(result).toBe(TEXT);
 });
+
+it('returns `null` if no `error` render provided given an `ErrorAsync`', () => {
+  const result = render(newError(), {});
+  expect(result).toBeNull();
+})
